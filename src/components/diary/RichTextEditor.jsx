f906b490b2b16d2814ef5e2ecf70bcf50b3c19af
Hieu <Hieu@DESKTOP-MTTTFAG>
2022-12-04 16:06:04 +0700

    rich editor

--- src/components/diary/RichTextEditor.jsx
+++ src/components/diary/RichTextEditor.jsx
@@ -1,24 +1,103 @@
-import { Dimensions, ScrollView, StyleSheet, View } from 'react-native';
+import { Alert, Dimensions, ScrollView, StyleSheet, View } from 'react-native';
 import { useRef, useState } from 'react';
 import { RichEditor, RichToolbar, actions } from 'react-native-pell-rich-editor';
+import * as ImagePicker from 'expo-image-picker';
+import axiosConfig from '../../utils/axios';
+import { getFileInfo, isLessThanTheMB } from '../../utils/checkFileSize';
 
 const screen = Dimensions.get('screen');
 
-function RichTextEditor() {
+const RichTextEditor = () => {
   const richText = useRef();
-
   const [contentHtml, setContentHtml] = useState(
     '<div>ùcuc uvigig ugigiv gvuv</div><div>uviviv hhhh<img src="https://i.pinimg.com/originals/6f/5a/34/6f5a34181beb2c2de8185d39255ad08a.jpg" style="font-size: 1em;"></div>',
   );
 
-  const richTextHandle = (descriptionText) => {
-    console.log(descriptionText);
+  const pickImage = async () => {
+    const gallery = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (gallery.granted === false) {
+      console.log('not has permissions');
+      return;
+    }
+
+    const imageResult = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      base64: true,
+      quality: 1,
+    });
+
+    if (imageResult.cancelled) return;
+
+    if (imageResult.type !== 'image') return;
+
+    const fileInfo = await getFileInfo(imageResult.uri);
+
+    if (!fileInfo?.size) {
+      Alert.alert('alert', "Can't select this file as the size is unknown.");
+      return;
+    }
+
+    const isLessThan1MB = isLessThanTheMB(fileInfo.size, 1);
+    if (!isLessThan1MB) {
+      Alert.alert('alert', 'Image size must be smaller than 1MB!');
+      return;
+    }
+
+    const time = new Date().getTime();
+    const data = {
+      ImageName: 'image' + time.toString() + '.png',
+      Base64String: imageResult?.base64,
+    };
+    const response = await axiosConfig.post('Authenticate/upload-image', data);
+    richText.current.insertImage(response.data);
   };
 
-  const addImage = () => {
-    richText.current.insertImage(
-      'https://i.pinimg.com/originals/6f/5a/34/6f5a34181beb2c2de8185d39255ad08a.jpg',
-    );
+  const openCamera = async () => {
+    const camera = await ImagePicker.requestCameraPermissionsAsync();
+    if (camera.granted === false) {
+      console.log('Camera not granted');
+      return;
+    }
+
+    const imageResult = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      base64: true,
+      quality: 1,
+    });
+
+    if (imageResult.cancelled) return;
+
+    if (imageResult.type !== 'image') {
+      Alert.alert('','Hình ảnh không đúng định dạng');
+      return;
+    }
+
+    const fileInfo = await getFileInfo(imageResult.uri);
+
+    if (!fileInfo?.size) {
+      Alert.alert('','Hình ảnh không hợp lệ');
+      return;
+    }
+
+    const isLessThan1MB = isLessThanTheMB(fileInfo.size, 1);
+    if (!isLessThan1MB) {
+      Alert.alert('','Hình ảnh phải có dung lượng nhỏ hơn 1MB!');
+      return;
+    }
+
+    const time = new Date().getTime();
+    const data = {
+      ImageName: 'test' + time.toString() + '.png',
+      Base64String: imageResult?.base64,
+    };
+    const response = await axiosConfig.post('Authenticate/upload-image', data);
+    richText.current.insertImage(response.data);
+  };
+
+  const richTextHandle = (descriptionText) => {
+    console.log(descriptionText);
   };
 
   return (
@@ -50,13 +129,13 @@ function RichTextEditor() {
           actions.fontSize,
           actions.keyboard,
         ]}
-        onPressAddImage={addImage}
-        insertVideo={addImage}
+        onPressAddImage={pickImage}
+        insertVideo={openCamera}
         style={styles.richTextToolbarStyle}
       />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   richTextContainer: {
@@ -80,7 +159,8 @@ const styles = StyleSheet.create({
     borderColor: '#c6c3b3',
     borderRadius: 10,
     borderWidth: 1,
-    color: '#fff'
+    color: '#fff',
   },
 });
+
 export default RichTextEditor;
